c5ffba131a6cf55ad250a72224ca2803ab108195
EdinNicolas-web <edin_albarracin@outlook.com>
2023-03-01 20:17:10 -0500

    Finish view bands

---
+++ lib/models/band.dart
@@ -0,0 +1,17 @@
+class Band {
+  Band({
+    required this.id,
+    required this.name,
+    required this.votes,
+  });
+
+  final String id;
+  final String name;
+  final int votes;
+
+  factory Band.fromMap(Map<String, dynamic> obj) => Band(
+        id: obj['id'],
+        name: obj['name'],
+        votes: obj['votes'],
+      );
+}
